45a92a04b65f0803c26903501ebe67f7957f643a
Robin Feng <fengliubin@126.com>
2016-03-15 10:14:56 +0800

    release to websit

--- nmrcen/settings.py
+++ nmrcen/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')9^)1yl#w#)4_o+nsy5s&0tp&(yn@l_t37t=ovwhf^n6m-i4*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'nmrcen.xmu.edu.cn']
+ADMINS = [('FengLiubin', 'fengliubin@126.com'),]
 
 # Application definition
 
@@ -113,9 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join( BASE_DIR, "static" )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join( BASE_DIR, 'media' )
--- static/manual/anonymousmanual.md
+++
@@ -1,1 +0,0 @@
-# Registed Manual
